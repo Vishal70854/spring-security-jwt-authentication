2c678994ec113a1223f7503eba453bdf4c9e7cf8
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2026-01-30 15:39:54 +0530

    implemented UserDetailsService to load the username from db

--- src/main/java/com/amigos/security/config/ApplicationConfig.java
+++ src/main/java/com/amigos/security/config/ApplicationConfig.java
@@ -1,4 +1,24 @@
 package com.amigos.security.config;
 
+import com.amigos.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
 }
+
